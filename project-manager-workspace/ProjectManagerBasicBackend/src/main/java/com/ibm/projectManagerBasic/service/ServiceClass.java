e99482003c8681282fbaa57e816470a9e992cfeb
TejuKanchu <TejuKanchu@DESKTOP-GMMREGF>
2021-03-18 08:20:19 +0530

    implementation of view data and password validation in service class

--- project-manager-workspace/ProjectManagerBasicBackend/src/main/java/com/ibm/projectManagerBasic/service/ServiceClass.java
+++ project-manager-workspace/ProjectManagerBasicBackend/src/main/java/com/ibm/projectManagerBasic/service/ServiceClass.java
@@ -1,6 +1,7 @@
 package com.ibm.projectManagerBasic.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,4 +36,27 @@ public class ServiceClass {
 	public void updateUserDetails(UserDetails user, Integer id) {
 		repo.updateUserDetails(user.getUserName(), user.getUserPassword(), user.getUserDesignation(), id);
 	}
+	
+
+//	 view the user Information by using id
+	public Optional<UserDetails> viewUserDetails(Integer id) {
+		
+	          return repo.findById(id); 
+	}
+
+	
+	
+//	  Validate the user password from database
+	public boolean validatePassword(UserDetails user) {
+		String pwd=repo.findByUserName(user.getUserName());
+	    boolean flag=pwd.equals(user.getUserPassword());
+	    return flag;
+			
+		
+		
+	}
+		
+		
+		
+	
 }
